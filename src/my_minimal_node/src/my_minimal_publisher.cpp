e5577b90bafa4f9d91e1f799e735875c01a7421f
rjmSTL <ravi.mali@sirabtech.com>
2021-04-14 17:43:43 +0530

    Add delay of 1 sec

--- src/my_minimal_node/src/my_minimal_publisher.cpp
+++ src/my_minimal_node/src/my_minimal_publisher.cpp
@@ -6,15 +6,18 @@ int main (int argc, char **argv){
 	ros::init(argc, argv, "minimal_publisher");
 	ros::NodeHandle n;
 
-	ros::Publisher my_publisher_obj = n.advertise <std_msgs::Float64> ("test_topic", 1); // test_topic = name of topic. 1 is buffer size
+	// test_topic = name of topic. 1 is buffer size
+	ros::Publisher my_publisher_obj = n.advertise <std_msgs::Float64> ("test_topic", 1); 
 
 	std_msgs::Float64 op_data;
+	ros::Rate delay = 1.0;
 
 	op_data.data = 0.0;
 	while (ros::ok())
 	{
 		op_data.data = op_data.data + 0.001;
 		my_publisher_obj.publish(op_data);
+		delay.sleep();
 	}
 	return 0;
 }
